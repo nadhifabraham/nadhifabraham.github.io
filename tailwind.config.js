516027f0f4b4d3aa68a713af0681f7fecf42faa0
nadhifabraham <nadhifnafiza77@gmail.com>
2025-02-09 07:40:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors: {
+      primary:'#4f46e5',
+      dark: '#27272a'
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
